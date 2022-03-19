42867634541884c0b5ce9b2ee5fde234782270b1
hhl64138 <49326296+hhl64138@users.noreply.github.com>
2022-03-18 23:59:31 -0700

    Update calendar

--- app/Http/Livewire/Calendar.php
+++ app/Http/Livewire/Calendar.php
@@ -8,15 +8,28 @@ use Carbon\Carbon;
 
 class Calendar extends LivewireCalendar
 {
+
     public function events() : Collection
     {
         return collect([
             [
                 'id' => 1,
-                'title' => 'Prueba',
-                'description' => 'Prueba',
+                'title' => 'Breakfast',
+                'description' => 'Pancakes! 🥞',
                 'date' => Carbon::today(),
-            ]
+            ],
+            [
+                'id' => 2,
+                'title' => 'Meeting with Pamela',
+                'description' => 'Work stuff',
+                'date' => Carbon::tomorrow(),
+            ],
         ]);
     }
+
+    public function onEventClick($eventId)
+    {
+        echo 'test';
+    }
+
 }
